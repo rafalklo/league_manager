18f34e0acd4050bf369ba28e5d02e5011908948e
Rafal Klodzinski <rafal@utxsolutions.com>
2014-07-06 12:54:09 -0400

    Add LeagueManager::Member methods

---
+++ spec/league_manager/member_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe LeagueManager::Member do
+
+  context '.controller_name' do
+    it 'returns the name of the remote contoller' do
+      object = LeagueManager::Member
+      expect(object.controller_name).to eq("members")
+    end
+  end
+
+  context '.get', :vcr do
+
+    context '#index' do
+      it 'fetches all active mvp players' do
+        result = LeagueManager::Member.get({:method => :mvp})
+        expect(result.size).to eq(15)
+        expect(result.first.player.member.name).to eq("Hennessy, Steve")
+      end
+
+    end
+
+    context  '#search' do
+      it 'searches for a member by first and last name' do
+        result = LeagueManager::Member.get({:method => :search, :q => "rafal"})
+        expect(result.first.firstname).to eq("Rafal")
+        expect(result.first.lastname).to eq("Klodzinski")
+      end
+    end
+
+  end
+end
