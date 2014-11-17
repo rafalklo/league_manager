33e6fd95299f6352045806f0cc04a060f0864a92
Rafal Klodzinski <rafal@utxsolutions.com>
2014-11-16 19:48:46 -0500

    Add published_at to content

--- lib/league_manager/content.rb
+++ lib/league_manager/content.rb
@@ -1,10 +1,15 @@
 module LeagueManager
   class Content < LeagueManager::Base
-    attr_reader :title_en, :active, :en, :fr, :title_fr
+    attr_reader :title_en, :active, :en, :fr, :title_fr, :created_at
     class << self
       def controller_name
         "infos"
       end
     end
+
+    def published_at
+      self.created_at
+    end
+
   end
 end
