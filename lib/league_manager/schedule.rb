a9f1a871cf6493a485f3aa2dc368ff6f3b23df09
Rafal Klodzinski <rafal@utxsolutions.com>
2014-12-09 07:06:12 -0500

    Add team record to schedule

--- lib/league_manager/schedule.rb
+++ lib/league_manager/schedule.rb
@@ -1,7 +1,7 @@
 module LeagueManager
   class Schedule < LeagueManager::Base
     attr_accessor :home_team_name, :home_score, :away_team_name, :away_score, :game_defaulted, :game_date, :game_time, :home_id, :away_id, :is_playoffs, :playoffs_category, :played, :field_name
-    attr_accessor :gamesheets
+    attr_accessor :gamesheets, :home_team_record, :away_team_record
 
     def gamesheets
       @gamesheets || []
