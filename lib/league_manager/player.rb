2443965239f2030fe3eab1088942a4250d767389
Rafal Klodzinski <rafal@utxsolutions.com>
2014-12-09 07:17:41 -0500

    Add points and assists to Player

--- lib/league_manager/player.rb
+++ lib/league_manager/player.rb
@@ -1,6 +1,6 @@
 module LeagueManager
   class Player < LeagueManager::Base
-    attr_reader :member, :team, :goals, :yellows, :double_yellows, :reds, :mvp_awards, :games
+    attr_reader :member, :team, :goals, :yellows, :double_yellows, :reds, :mvp_awards, :games, :assists, :points
     # Goalie season stats
     attr_reader :goalie_goals_against, :goalie_games, :goalie_goals_against_average, :goalie_shutouts
   end
