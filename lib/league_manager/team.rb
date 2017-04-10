ee63a81845a011ede6f4524ed7a51208ec5aa5a3
Rafal Klodzinski <rafalklo@gmail.com>
2017-04-10 14:17:01 -0400

    Add picture_path to Team

--- lib/league_manager/team.rb
+++ lib/league_manager/team.rb
@@ -2,5 +2,6 @@ module LeagueManager
   class Team < LeagueManager::Base
     attr_reader :color, :division_name, :league_name, :season_name, :active, :division
     attr_reader :players, :standing, :season_champions, :playoffs_champions
+    attr_reader :picture_path
   end
 end
