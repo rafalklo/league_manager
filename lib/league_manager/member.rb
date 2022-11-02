d7d337d5fd63ac6814c8799c18518b698887d018
Rafal Klodzinski <rafalklo@gmail.com>
2022-11-02 09:02:21 -0400

    Add blacklisted attribute to member, bump to 1.6.1

--- lib/league_manager/member.rb
+++ lib/league_manager/member.rb
@@ -1,7 +1,7 @@
 module LeagueManager
   class Member < LeagueManager::Base
     attr_reader :firstname, :lastname, :players, :created_at, :name
-    attr_reader :picture_path
+    attr_reader :picture_path, :is_blacklisted
 
     def self.show id
       get({:id => id})
